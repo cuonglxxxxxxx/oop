c251ad7ce3c4a8456141756806257b2212ef0be4
cuonglxxxxxxx <147698453+cuonglxxxxxxx@users.noreply.github.com>
2024-08-03 21:59:14 +0700

    b1_slide2

---
+++ src/main/java/com/b1/B1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.b1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class B1 {
+    public static void main(String[] args) {
+        Circle cr1=new Circle();
+        cr1.setBanKinh(2);
+        cr1.inThongTin();
+        Rectangle rt1=new Rectangle();
+        rt1.setChieuDai(8);
+        rt1.setChieuRong(4);
+        rt1.inThongTin();
+    }
+}
