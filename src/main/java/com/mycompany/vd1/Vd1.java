d85a4cf84d0a46070a8033e9ef786e0a75b1d477
cuonglxxxxxxx <147698453+cuonglxxxxxxx@users.noreply.github.com>
2024-08-10 00:34:35 +0700

    video 05 bt

--- src/main/java/com/mycompany/vd1/Vd1.java
+++ src/main/java/com/mycompany/vd1/Vd1.java
@@ -9,41 +9,14 @@ import javax.swing.JOptionPane;
  * @author Lenovo
  */
 public class Vd1 {
-public static void change(SinhVien sv){
-  sv.settenSinhVien("Cuong");
+public static int plus (int ... arr){
+int kq=0;
+for(int i:arr){
+kq+=i;
 }
-/*
-public static void change (SinhVien sv){
-sv=new SinhVien("Cuong",2004,"Thanh hoa",1.74,64);
-}*/
-public static void main(String args[]) {
-    //lop bao
-SinhVien sv=new SinhVien("Hieu",2007,"Thanh Hoa",1.6,50);
-Float fl=new Float("25.6");
-float f=fl.floatValue();
-int i=fl.intValue();
-int j = Integer.parseInt("123");//parse tra ve kieu nguyen thuy int
-int k=Integer.valueOf("123");//valueOf tra ve doi tuong Integer
-int m=new Integer(Integer.MAX_VALUE);
-//System.out.println(m);
-System.out.println(Integer.MAX_VALUE);
-String input = "test 1-2-3";
-int output = 0;
-for (int index=0;index<input.length();index++) {
-char c = input.charAt(index);
-if (Character.isDigit(c))
-output = output * 10 + Character.digit(c, 10);// Character.digit(c, 10) chuyen ki tu c sang he thap phan (he 10)
+return kq;
 }
-System.out.println(output);
-    System.out.println("tong: "+1+2+3);
-    System.out.println("tong: "+(1+2+3));
-    System.out.println("Qua "+"dat "+"nay "+"la "+"cua "+"bo "+"may ");
-String name="DSK DE SUN KID";
-        System.out.println(name.charAt(4));
-   StringBuffer buffer=new StringBuffer(15);
-   buffer.append("DE ");
-   buffer.append("KID ");
-   buffer.insert(4, "SUN ");
-    System.out.println(buffer);
-}
+    public static void main(String[] args) {
+        System.out.println(plus(1,2,3,4,5));
+    }
 }
