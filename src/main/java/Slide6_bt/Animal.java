867ddcda7e7e43786fe0bfb709f6080fd1f423fa
cuonglxxxxxxx <147698453+cuonglxxxxxxx@users.noreply.github.com>
2024-08-28 16:55:37 +0700

    Lap trinh tong quat

---
+++ src/main/java/Slide6_bt/Animal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Slide6_bt;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Animal {
+    public static void eat(){
+        System.out.println("Eating...");
+    }
+}
+
