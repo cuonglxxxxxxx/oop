952575ac371c302a5d047117182e5715952e512c
cuonglxxxxxxx <147698453+cuonglxxxxxxx@users.noreply.github.com>
2024-09-13 23:32:56 +0700

    Comparator

---
+++ src/main/java/Slide6_bt/Person.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Slide6_bt;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Person {
+    private int age;
+    private String name;
+    public void setAge(int age){
+    this.age=age;
+    }
+    public int getAge(){
+    return this.age;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
